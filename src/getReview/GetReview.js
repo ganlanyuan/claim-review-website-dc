4b64553e9d32ad8b0e6acd0011d01ce4d0312da2
William Lin 22 <ganlanyuan@users.noreply.github.com>
2021-03-12 13:35:03 -0500

    update styles

--- src/getReview/GetReview.js
+++ src/getReview/GetReview.js
@@ -104,7 +104,7 @@ class GetReview extends Component {
         </div>
         <div className="panel">
           <div className="toptitle">
-            <h1>Claim <strong>Your Benefit</strong> Now!!!</h1>
+            <h1>Claim Your Benefit Now!!!</h1>
           </div>
           <div className="tablist">
             <div id="order" className={this.props.step_info.length > 1 ? "tabunit passedTab" : "tabunit activeTab" }>
